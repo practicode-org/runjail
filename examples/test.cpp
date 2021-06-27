5fee6f073cf8c73d9c235c971b1708019f54c347
Alexander Garustovich <yukigaru@gmail.com>
2021-06-27 10:21:27 +0300

    Added /examples directory

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-
+#include <vector>
+#include <ctime>
+#
 int main() {
+    std::vector<int> v;
+
     for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 100000; j++) {
+            v.push_back((int)std::time(nullptr));
+        }
         std::cerr << i + 1 << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(600));
     }
     std::cout << "Hello\", world!" << std::endl;
-    return 1;
+    return (v.size() + v.back()) % 60;
 }
